08bec41623d14b7c710e09eb8ae7e6f4b317f5a6
VARGHESE P ROY <125295191+Varghese254@users.noreply.github.com>
2024-11-21 06:13:38 +0530

    created seperate js files

---
+++ example.js
@@ -0,0 +1,12 @@
+//data types
+//number
+let num = 10;
+console.log(num);
+
+//string
+let str="varghese";
+console.log(str);
+
+//object
+const person={firstname:"john",lastname:"doe"};
+console.log(person);
