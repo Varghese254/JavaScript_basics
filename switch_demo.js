484cb0aeae6cc44d2b424d33110e8db3bbe04c30
VARGHESE P ROY <125295191+Varghese254@users.noreply.github.com>
2024-11-21 20:43:25 +0530

    switch statements

---
+++ switch_demo.js
@@ -0,0 +1,13 @@
+let day=3;
+switch(day){
+    case 0:
+        console.log("monday");
+        break;
+
+    case 1:
+        console.log("tuesday");
+        break;
+    case 3:
+        console.log("wednesday");
+        break; 
+}
